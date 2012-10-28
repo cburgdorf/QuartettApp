d56cf727a17ec8584aa014266c6c8b343ce98c7c
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2012-10-28 23:57:02 +0100

    some UI love

--- app/view/GameView.js
+++ app/view/GameView.js
@@ -23,7 +23,7 @@ Ext.define("QuartettApp.view.GameView", {
                     //Todo: Currently we hard code those. Would be better to have generic title and subtitle on the card
                     //and render those with a different visual style
                     '<tpl if="key !== \'name\' && key !== \'carmaker\'">',
-                        '<div data-key="{key}" class="x-button"><span data-key="{key}" class="x-button-label">Play!</span></div>',
+                        '<div data-key="{key}" class="x-button x-button-play"><span data-key="{key}" class="x-button-label">Play!</span></div>',
                     '</tpl>',
                 '</div>',
                 '</tpl>',
